d6fdb16dd4548501bbb57dd9cb0bd0a44fc400a4
Danyal Faheem <danyalfaheem@gmail.com>
2026-01-25 02:53:20 +0400

    Initial Commit

---
+++ Zaman/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Zaman
+//
+//  Created by Danyal Faheem on 25/01/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Zaman/ZamanApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZamanApp.swift
+//  Zaman
+//
+//  Created by Danyal Faheem on 25/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct ZamanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
